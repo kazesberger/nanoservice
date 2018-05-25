eaa5927899e39fc27167aad252ef0db063c8ae35
kazesberger <klaus.azesberger@bearingpoint.com>
2018-05-25 13:53:40 +0200

    wip

--- src/nanoservice/core.clj
+++ src/nanoservice/core.clj
@@ -2,25 +2,25 @@
   (:require [ring.adapter.jetty :as jetty]
             [cheshire.core :as cheshire]
             [ring.middleware.json :refer [wrap-json-response]]
-            [ring.util.response :refer [response]]))
+            [ring.util.response :refer [response content-type]]))
 
-def db (atom {})
+(def db (atom {}))
 
-(defn resp-handler
-  (response
-    (cheshire/generate-string {:name       "klaus"
-                               :profession "HROO! HROO! HROO!"
-                               :foo        ["bar" "baz"]})))
+(defn resp-handler [request]
+  (content-type
+    (response
+      (cheshire/generate-string {:name       "klaus"
+                                 :profession "HROO! HROO! HROO!"
+                                 :foo        ["bar" "baz"]}))
+    "application/json"))
+
+(response "Hello World")
 
 (defn handler [request]
   {:status  200
    :headers {"Content-Type" "application/json"}
-   :body    (cheshire/generate-string {:name       "klaus"
-                                       :profession "HROO! HROO! HROO!"
-                                       :foo        ["bar" "baz"]})})
+   :body    "foo"})
 
 
-;(jetty/run-jetty handler {:port 3000
-;                          :async? true
-;                          :join? false
-;                          :async true})
+(jetty/run-jetty resp-handler {:port 3000
+                               :join? false})
