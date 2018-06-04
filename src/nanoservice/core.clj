7dacdb8c7df8a35c8a2f7bbd77c805ea94f6386b
kazesberger <klaus.azesberger@bearingpoint.com>
2018-06-04 18:18:22 +0200

    wip-expload-zip-file

--- src/nanoservice/core.clj
+++ src/nanoservice/core.clj
@@ -25,9 +25,21 @@
   (swap! db f)
   (state))
 
+;(defn upload-handler)
+
+(def db2 (atom {}))
+
+
+; https://stackoverflow.com/questions/32742744/how-to-download-a-file-and-unzip-it-from-memory-in-clojure
+; https://www.eclipse.org/jetty/javadoc/current/org/eclipse/jetty/server/HttpInput.html
+; from debug: {:ssl-client-cert nil, :protocol "HTTP/1.1", :cookies {}, :remote-addr "127.0.0.1", :params {}, :flash nil, :route-params {}, :headers {"user-agent" "curl/7.47.0", "host" "localhost:3000", "accept" "*/*", "content-length" "276696", "expect" "100-continue"}, :server-port 3000, :content-length 276696, :form-params {}, :compojure/route [:put "/serenity"], :session/key nil, :query-params {}, :content-type nil, :character-encoding nil, :uri "/serenity", :server-name "localhost", :query-string nil, :body #object[org.eclipse.jetty.server.HttpInputOverHTTP 0x15f925d4 "HttpInputOverHTTP@15f925d4"], :multipart-params {}, :scheme :http, :request-method :put, :session {}}
+
 (comp/defroutes main-routes
   (comp/GET "/inc" [] (handler inc))
   (comp/GET "/dec" [] (handler dec))
+  (comp/GET "/serenity" [] (handler dec))
+  (comp/PUT "/serenity" request (reset! db2 request))
+  (comp/GET "/debug" [] (str @db2))
   (route/resources "/")
   (route/not-found "Page not found"))
 
